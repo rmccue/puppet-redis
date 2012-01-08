237d447de7a0ab91588e1e68cb3d4648e12b7799
Eivind Uggedal <eivind@uggedal.com>
2012-01-08 15:56:08 +0100

    Update tar version from 2.4.4 to 2.4.5.

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,6 +1,6 @@
 class redis::server($ensure=present,
                     $version='2.4.5',
-                    $tar_version="2.4.4",
+                    $tar_version="2.4.5",
                     $bind="127.0.0.1",
                     $port=6379,
                     $masterip="",
